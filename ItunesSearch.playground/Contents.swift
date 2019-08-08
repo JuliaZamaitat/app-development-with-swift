53cb6ecdde025dd78b68278b1e8393488966cb1d
Julia Zamaitat <j.zamaitat@gmail.com>
2019-08-08 21:21:09 +0200

    5.4 Itunes Search finished.

--- ItunesSearch.playground/Contents.swift
+++ ItunesSearch.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var str = "Hello, playground"
+import PlaygroundSupport
+import Foundation
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.compactMap { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
+
+
+let baseURL = URL(string: "https://itunes.apple.com/search")!
+
+let query = [
+    "term": "james blake",
+    "media": "music",
+    "limit": "200"
+]
+
+let url = baseURL.withQueries(query)!
+
+let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    if let data = data,
+        let result = String(data: data, encoding: .utf8) {
+        print(result)
+        
+        PlaygroundPage.current.finishExecution()
+    }
+}
+
+task.resume()
